cfd527ee073a2c28e2a61b6d3ba967602f4d1eb6
Stuart Olsen <so1132@txstate.edu>
2014-12-12 14:13:16 -0600

    Successfully running 64-bit code

--- kernel_main.c
+++ kernel_main.c
@@ -1,3 +1,19 @@
+#include <stdint.h>
+#include "vga/tinyvga.h"
+
+static tinyvga vga;
+
 void kernel_main (void)
 {
+	register uint64_t tmp = 0;
+	__asm__ volatile (
+		"movq $(1 << 48), %0;"
+		"shr $32, %0;"
+		: "+r" (tmp)
+	);
+
+	vga = vga_initialize ();
+	vga_clear (&vga);
+	vga_putline (&vga, "64-bit boot successful!");
+	vga_putline (&vga, (tmp == (1 << 16) ? "GOOD" : "BAD"));
 }
