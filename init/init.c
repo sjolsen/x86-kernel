e2849b01d438b9451d5aa5c59db93feefc9ff048
Stuart Olsen <so1132@txstate.edu>
2014-12-12 17:18:18 -0600

    Removed unnecessary stuff from init

--- init/init.c
+++ init/init.c
@@ -1,15 +1,8 @@
-#include "vga/tinyvga.h"
-#include "x86/cpuid.h"
 #include "x86/GDT.h"
 #include "x86/paging.h"
-#include "x86/interrupts/ISR.h"
-#include "x86/interrupts/IDT.h"
-#include "x86/interrupts/IRQ.h"
 #include <stdbool.h>
 
-static tinyvga vga;
 static GDT gdt;
-static TSS_64 tss;
 static PML4_table pml4_table;
 static PDP_table pdp_table;
 static Page_directory page_directory;
@@ -18,7 +11,13 @@ static Page_directory high_page_directory;
 static inline
 bool capable_64 (void)
 {
-	return cpuid (0x80000001).EDX & (1 << 29);
+	register uint32_t tmp = 0;
+	__asm__ volatile (
+		"mov $0x80000001, %%eax;"
+		"cpuid;"
+		: "=d" (tmp)
+	);
+	return tmp & (1 << 29);
 }
 
 extern const void _ktext_base;
@@ -147,108 +146,20 @@ void enable_paging (void)
 	);
 }
 
-static
-void debug_halt_ISR (__attribute__ ((unused)) INT_index interrupt)
+void init (void)
 {
-	static const char* name [] = {
-		[0x00] = "divide_by_zero",
-		[0x01] = "debugger",
-		[0x02] = "NMI",
-		[0x03] = "breakpoint",
-		[0x04] = "overflow",
-		[0x05] = "bounds",
-		[0x06] = "invalid_opcode",
-		[0x07] = "coprocessor_unavailable",
-		[0x08] = "double_fault",
-		[0x0A] = "invalid_TSS",
-		[0x0B] = "segment_missing",
-		[0x0C] = "stack_fault",
-		[0x0D] = "protection_fault",
-		[0x0E] = "page_fault",
-		[0x10] = "math_fault",
-		[0x11] = "alignment_check",
-		[0x12] = "machine_check",
-		[0x13] = "SIMD_exception",
-
-		[0x20] = "IRQ_PIT",
-		[0x21] = "IRQ_keyboard",
-		[0x22] = "IRQ_cascade",
-		[0x23] = "IRQ_COM2",
-		[0x24] = "IRQ_COM1",
-		[0x25] = "IRQ_LPT2",
-		[0x26] = "IRQ_floppy",
-		[0x27] = "IRQ_LPT1",
-		[0x28] = "IRQ_CMOS_RTC",
-		[0x29] = "IRQ_misc1",
-		[0x2A] = "IRQ_misc2",
-		[0x2B] = "IRQ_misc3",
-		[0x2C] = "IRQ_mouse",
-		[0x2D] = "IRQ_FPU",
-		[0x2E] = "IRQ_HDD1",
-		[0x2F] = "IRQ_HDD2",
-	};
-
-	vga_put (&vga, "Interrupt: ");
-	vga_putline (&vga, name [interrupt]);
-
-	__asm__ volatile (
-		".halt:"
+	if (!capable_64 ())
+		__asm__ volatile (
+			".halt:"
 			"cli;"
 			"hlt;"
 			"jmp .halt"
-	);
-}
+		);
 
-void initialize_interrupts (void)
-{
-	IDT_initialize ();
-	ISR_table_initialize (&debug_halt_ISR);
-	IRQ_disable (IRQ_PIT);
-	__asm__ ("sti");
-}
-
-void init (void)
-{
-	vga = vga_initialize ();
-	vga_clear (&vga);
-
-	if (capable_64 ())
-		vga_putline (&vga, "64-bit capable.");
-	else {
-		vga_putline (&vga, "Not 64-bit capable.");
-		return;
-	}
-
-	GDT_initialize (&gdt, &tss);
-	vga_putline (&vga, "GDT initialized.");
-
-	initialize_interrupts ();
-	vga_putline (&vga, "Interrupt table initialized.");
-
+	GDT_initialize (&gdt);
 	paging_initialize ();
-	vga_putline (&vga, "Page table initialized.");
-
 	enable_PAE ();
-	vga_putline (&vga, "PAE bit set.");
-
 	load_PML4 (&pml4_table);
-	vga_putline (&vga, "PML4 table loaded.");
-
 	enable_LM ();
-	vga_putline (&vga, "Long mode enabled.");
-
 	enable_paging ();
-	vga_putline (&vga, "Paging enabled.");
-
-	/* for (int i = 0; i < 100; ++i) */
-	/* { */
-	/* 	static const char tbl [] = "0123456789ABCDEF"; */
-
-	/* 	const uint8_t b = ((uint8_t*)&_ktext_start) [i]; */
-	/* 	const uint8_t b0 = b & 0xF; */
-	/* 	const uint8_t b1 = b >> 4; */
-	/* 	vga_putchar (&vga, tbl [b1]); */
-	/* 	vga_putchar (&vga, tbl [b0]); */
-	/* 	vga_putchar (&vga, ' '); */
-	/* } */
 }
--- init/x86/GDT.h
+++ init/x86/GDT.h
@@ -8,7 +8,6 @@ enum {
 	KERNEL_INIT_SELECTOR = 1,
 	KERNEL_DATA_SELECTOR = 2,
 	KERNEL_TEXT_SELECTOR = 3,
-	TSS_SELECTOR = 4
 };
 
 typedef struct __attribute__ ((packed)) __attribute__ ((aligned (8))) {
@@ -31,33 +30,10 @@ typedef struct __attribute__ ((packed)) __attribute__ ((aligned (8))) {
 } GDT_entry;
 _Static_assert (sizeof (GDT_entry) == 8, "GDT_entry not packed");
 
-typedef struct __attribute__ ((packed)) __attribute__ ((aligned (8))) {
-	uint16_t limit_low;
-	uint16_t base_low;
-	uint8_t  base_high1;
-	uint8_t  type        : 4;
-	uint8_t  zero0       : 1; // Must be zero
-	uint8_t  privilege   : 2;
-	uint8_t  present     : 1;
-	uint8_t  limit_high  : 4;
-	uint8_t              : 1;
-	uint8_t  zero1       : 2; // Must be zero
-	uint8_t  granularity : 1;
-	uint8_t  base_high2;
-	uint32_t base_high3;
-	uint32_t reserved; // Must be zero
-} TSS_descriptor;
-
 typedef struct __attribute__ ((packed)) {
 	GDT_entry GDTEs [4];
-	TSS_descriptor TSSD;
 } GDT;
-_Static_assert (sizeof (GDT) == (4 * sizeof (GDT_entry) + sizeof (TSS_descriptor)), "GDT not packed");
 
-typedef struct __attribute__ ((packed)) {
-	uint8_t data [104];
-} TSS_64;
-
-void GDT_initialize (GDT* gdt, TSS_64* tss);
+void GDT_initialize (GDT* gdt);
 
 #endif
