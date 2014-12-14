08e35c0426c08d748dcbe962322b1111d7f28b59
Stuart Olsen <so1132@txstate.edu>
2014-12-14 16:35:04 -0600

    Use 64-bit code to print boot message

--- kernel_main.c
+++ kernel_main.c
@@ -3,12 +3,13 @@
 
 void kernel_main (void)
 {
-	const char* str = "Success.";
-
-	volatile uint16_t* vga_buffer = (volatile uint16_t*) 0xB8000;
-	size_t i = 0;
-	for (; i < 8; ++i)
-		vga_buffer [i] = (uint16_t)(str[i]) | ((uint16_t)7 << 8) | ((uint16_t)0) << 12;
-	for (; i < 80 * 25; ++i)
-		vga_buffer [i] = (uint16_t)' ' | ((uint16_t)7 << 8) | ((uint16_t)0) << 12;
+	uint64_t tmp = 0;
+	__asm__ volatile (
+		"movq $0x0763076307750753, %0;"
+		"movq %0, 0(%1);"
+		"movq $0x072e077307730765, %0;"
+		"movq %0, 8(%1)"
+		: "+r" (tmp)
+		: "r" (0xB8000)
+	);
 }
