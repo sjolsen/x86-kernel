ad07830160714fc60ca0e84ef693d9c3feb7282d
Stuart Olsen <so1132@txstate.edu>
2015-06-08 03:16:47 -0500

    Added flag to disable red zone in 64-bit code, just in case

--- toolchain.mk
+++ toolchain.mk
@@ -5,7 +5,7 @@ override AS64FLAGS:=$(ASFLAGS) $(AS64FLAGS) --64
 CC = gcc
 override CFLAGS:=$(CFLAGS) -I. -std=gnu99 -ffreestanding -Os -Wall -Wextra -Werror
 override C32FLAGS:=$(CFLAGS) $(C32FLAGS) -march=i686 -m32
-override C64FLAGS:=$(CFLAGS) $(C64FLAGS) -m64 -mcmodel=large
+override C64FLAGS:=$(CFLAGS) $(C64FLAGS) -m64 -mcmodel=large -mno-red-zone
 
 LD = ld
 override LDFLAGS:=$(LDFLAGS) -nostdlib
