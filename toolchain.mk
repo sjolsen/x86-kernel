66ae04d8b3cdf36c85dc6cc5a0767d32c2e0b5c7
Stuart Olsen <stuart@sj-olsen.com>
2024-01-14 20:12:37 -0600

    Add flags to suppress new spurious warnings.
    --param=min-pagesize=0 is now necessary when compiling code that accesses
    literal addresses to prevent GCC from warning about possible null pointer
    accesses. The logic seems to be that any address within the zero page is assumed
    to be derived from a null pointer; setting the page size to zero prevents this
    warning from triggering.
    
    -z noexecstack is now necessary to prevent LD from complaining about executable
    stacks. LD relies on a special section called .note.GNU-stack to mark functions
    as not requiring executable stacks. Forcibly turning off executable stack
    support suffices to suppress the warning.

--- toolchain.mk
+++ toolchain.mk
@@ -7,12 +7,12 @@ override AS32FLAGS:=$(ASFLAGS) $(AS32FLAGS) -march=i686 --32
 override AS64FLAGS:=$(ASFLAGS) $(AS64FLAGS) --64
 
 CC = gcc
-override CFLAGS:=$(CFLAGS) -I. -std=gnu99 -ffreestanding -fno-asynchronous-unwind-tables -fno-pie -ffunction-sections -fdata-sections -mno-sse -Os -g -Wall -Wextra -Werror
+override CFLAGS:=$(CFLAGS) -I. -std=gnu99 -ffreestanding -fno-asynchronous-unwind-tables -fno-pie -ffunction-sections -fdata-sections -mno-sse --param=min-pagesize=0 -Os -g -Wall -Wextra -Werror
 override C32FLAGS:=$(CFLAGS) $(C32FLAGS) -march=i686 -m32
 override C64FLAGS:=$(CFLAGS) $(C64FLAGS) -m64 -mcmodel=kernel -mno-red-zone
 
 LD = ld
-override LDFLAGS:=$(LDFLAGS) -nostdlib --gc-sections
+override LDFLAGS:=$(LDFLAGS) -nostdlib -z noexecstack --gc-sections
 override LD32FLAGS:=$(LDFLAGS) $(LD32FLAGS) -march=i686 -melf_i386
 override LD64FLAGS:=$(LDFLAGS) $(LD64FLAGS)
 
