be3ad4ea65b93e135e27b63fcf71c7b8ddabc167
Stuart Olsen <stuart@sj-olsen.com>
2024-01-16 02:30:04 -0600

    Most of the ELF serialization logic

--- tools/elf.cc
+++ tools/elf.cc
@@ -1,6 +1,7 @@
 #include "elf.hh"
 
 #include <algorithm>
+#include <bit>
 
 #include "format.hh"
 #include "span.hh"
@@ -105,4 +106,132 @@ ObjectFile parse_elf(std::span<const unsigned char> raw) {
     return obj;
 }
 
-void write_elf(std::ostream& os, const ObjectFile& obj);
+
+namespace {
+
+class StringTable {
+private:
+    std::vector<char> _data = {'\0'};
+
+public:
+    size_t store(std::string_view s) {
+        size_t result = _data.size();
+        std::ranges::copy(s, std::back_inserter(_data));
+        _data.push_back('\0');
+        return result;
+    }
+
+    std::span<const char> get() const {
+        return _data;
+    }
+};
+
+
+class Allocator {
+public:
+    struct Entry {
+        std::span<const unsigned char> data;
+        size_t offset;
+    };
+
+private:
+    std::vector<Entry> _entries;
+    size_t _next = 0;
+
+public:
+    void allocate(std::span<const unsigned char> data,
+                  size_t size, size_t align) {
+        switch (std::popcount(align)) {
+        case 0:
+            break;
+        case 1:
+            _next = (_next + (align - 1)) & ~(align - 1);
+            break;
+        default:
+            _next = align * ((_next + (align - 1)) / align);
+        }
+        size_t offset = _next;
+        _next += size;
+        _entries.emplace_back(data, offset);
+    }
+
+    template <typename T>
+    void allocate(const T* data) {
+        auto p = reinterpret_cast<const unsigned char*>(data);
+        size_t len = sizeof(T);
+        allocate({p, len}, len, alignof(T));
+    }
+
+    template <typename T>
+    void allocate(std::span<T> data) {
+        auto p = reinterpret_cast<const unsigned char*>(data.data());
+        size_t len = data.size() * sizeof(T);
+        allocate({p, len}, len, alignof(T));
+    }
+
+    std::span<const Entry> entries() const { return _entries; }
+};
+
+template <typename... T>
+struct match : T... { using T::operator()...; };
+
+}  // namespace
+
+
+void write_elf(std::ostream& os, const ObjectFile& obj) {
+    Elf64_Ehdr elf;
+    std::vector<Elf64_Shdr> shdrs(obj.sections.size());
+
+    // Recompute the contents of .shstrtab
+    StringTable shstrtab;
+    std::vector<uint32_t> sh_names;
+    for (const Section& section : obj.sections) {
+        if (std::holds_alternative<Null>(section.data))
+            sh_names.push_back(0);
+        else
+            sh_names.push_back(shstrtab.store(section.name));
+    }
+
+    // Compute file offsets
+    Allocator a;
+    a.allocate(&elf);
+    a.allocate(std::span(shdrs));
+    for (const Section& section : obj.sections) {
+        match visitor {
+            [](const Null&) {},
+            [&](const Progbits& data) {
+                a.allocate(data.bytes, data.bytes.size(), section.align);
+            },
+            [&](const Nobits& data) {
+                a.allocate({}, 0, section.align);
+            },
+            [&](const Rel& data) {
+                a.allocate(data.rels);
+            },
+            [&](const Rela& data) {
+                a.allocate(std::span(data.relas));
+            },
+            [&](const Symtab& data) {
+                a.allocate(data.symbols);
+            },
+            [&](const Strtab& data) {
+                a.allocate(data.strings);
+            },
+            [&](const ShStrtab&) {
+                a.allocate(shstrtab.get());
+            },
+        };
+        std::visit(visitor, section.data);
+    }
+
+    // Populate the headers
+    // TODO
+
+    // Write out the bytes
+    // TODO
+
+    std::cout << std::hex;
+    for (const Allocator::Entry& entry : a.entries()) {
+        std::cout << entry.offset << " " << entry.data.size() << std::endl;
+    }
+}
